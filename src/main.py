fc31e5eed7b680109095c551e9c50c200d969799
Brian David <xbriandavid@gmail.com>
2022-08-03 23:17:57 -0700

    main function adapted for added classes

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 from APIUtil.APIDriver import APISetup
+from FBClient.StoreInterface import FirebaseRequest
+from LinkUtil.GoogleSheetLink import Link, SheetLinkFacade
+from datetime import date
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
 def main():
-
+    t = date(2022, 4, 1)
     MYAPISetUp = APISetup(os.environ["clientservicefilename"], os.environ["firebaseclientfilename"])
     MYAPISetUp.createSession()
     keys = MYAPISetUp.getSession()
+    FBR = FirebaseRequest(keys, SheetLinkFacade())
+    FBR.PushUpdate("NS Training", t)
     return 0
 
 if __name__ == "__main__":
