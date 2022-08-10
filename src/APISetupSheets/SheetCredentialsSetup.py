5082e9f46b212aef8d0c3b868c1ad0bb21d47a18
Brian David <xbriandavid@gmail.com>
2022-08-10 10:33:38 -0700

    Added MutualTLSChanell exception

--- src/APISetupSheets/SheetCredentialsSetup.py
+++ src/APISetupSheets/SheetCredentialsSetup.py
@@ -2,6 +2,7 @@ from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.errors import HttpError
 from google.oauth2.service_account import Credentials
+from google.auth.exceptions import MutualTLSChannelError
 
 import json
 
@@ -38,5 +39,5 @@ class SheetsCredentialConnectorConsumer:
             try:
                 service = build('sheets', 'v4', credentials=credentials)
                 return service
-            except HttpError as error:
-                return None
+            except MutualTLSChannelError:
+                return MutualTLSChannelError
