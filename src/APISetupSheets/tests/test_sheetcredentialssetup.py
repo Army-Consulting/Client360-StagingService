118b589f4c3f7c544845eee74ca40f5e8370b298
Brian David <xbriandavid@gmail.com>
2022-08-10 09:55:30 -0700

    added tests that catch specific exception

--- src/APISetupSheets/tests/test_sheetcredentialssetup.py
+++ src/APISetupSheets/tests/test_sheetcredentialssetup.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+import pytest
+from APISetupSheets.SheetCredentialsSetup import SheetsCredentialConnectorConsumer, SheetsCredentialConnectorService
 from APISetupSheets.SheetCredentialsSetup import SheetsCredentialConnectorService
 from google.oauth2.service_account import Credentials
 
@@ -6,17 +8,18 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-def test_SheetCredentialSetup_void_file():
-    # tests that a missing config file is considered an
-    # IO error
-    Sheetobject = SheetsCredentialConnectorService("test.jpg")
-    assert Sheetobject.credentialSetUp() == "IO-ERROR"
-
-def test_SheetCredential_incorrect_format():
-    # tests that incorrectly formatted Google Sheets credentials are caught
-    Sheetobject = SheetsCredentialConnectorService("../mocks/mock_sheets_credential.json")
-    assert Sheetobject.credentialSetUp() == "Value Error"
-
 def test_credentialSetUpRType():
     Sheetobject = SheetsCredentialConnectorService(os.environ["test_credentialSetUpRType"])
     assert type(Sheetobject.credentialSetUp()) is Credentials
+
+def test_SheetCredentialCCActivate_IncorrectRoot():
+    Sheetobject = SheetsCredentialConnectorService("test.jpg")
+    SCCS = SheetsCredentialConnectorConsumer(Sheetobject)
+    with pytest.raises(IOError) as e_info:
+        SCCS.activate()
+
+def test_SheetCredentialCCActivate_IncorrectFrmt():
+    Sheetobject = SheetsCredentialConnectorService("../mocks/mock_sheets_credential.json")
+    SCCS = SheetsCredentialConnectorConsumer(Sheetobject)
+    with pytest.raises(ValueError) as e_info:
+        SCCS.activate()
