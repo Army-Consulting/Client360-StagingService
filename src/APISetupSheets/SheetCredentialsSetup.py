cd10941b0ac42e4d630d4594d252f32a9e9b42a6
Brian David <xbriandavid@gmail.com>
2022-08-10 09:55:09 -0700

    Included custom exceptions -io/value

--- src/APISetupSheets/SheetCredentialsSetup.py
+++ src/APISetupSheets/SheetCredentialsSetup.py
@@ -1,6 +1,8 @@
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from googleapiclient.errors import HttpError
+from google.oauth2.service_account import Credentials
+
 import json
 
 class SheetsCredentialConnectorService:
@@ -12,27 +14,29 @@ class SheetsCredentialConnectorService:
         try:
             billing_project_credential_json = open(self.configfile)
         except IOError:
-            return "IO-ERROR"
-        except:
-            return "OTHER ERROR"
+            return IOError
         billing_project_account = json.load(billing_project_credential_json)
         try:
             credentials = service_account.Credentials.from_service_account_info(billing_project_account)
         except ValueError:
-            return "Value Error"
-
+            return ValueError
         scoped_credentials = credentials.with_scopes(['https://www.googleapis.com/auth/spreadsheets'])
         return scoped_credentials
 
 class SheetsCredentialConnectorConsumer:
 
-    def __init__(self, CredentialConnector):
+    def __init__(self, CredentialConnector: Credentials):
         self.credentialconnector = CredentialConnector
+
     def activate(self):
-        
         credentials = self.credentialconnector.credentialSetUp()
-        try:
-            service = build('sheets', 'v4', credentials=credentials)
-            return service
-        except HttpError as error:
-            return None
+        if(credentials == IOError):
+            raise IOError
+        elif(credentials == ValueError):
+            raise ValueError
+        else:
+            try:
+                service = build('sheets', 'v4', credentials=credentials)
+                return service
+            except HttpError as error:
+                return None
